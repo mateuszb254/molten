1ae974148dcf8a76b73f11ce92b5d1aee022b07c
Mateusz Bątruk <mateuszb254@gmail.com>
2018-07-12 13:52:29 +0200

    Label for name field [ShopCategoryType] added.

--- src/Form/ShopCategoryType.php
+++ src/Form/ShopCategoryType.php
@@ -13,7 +13,9 @@ class ShopCategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'label' => 'label.category'
+            ])
         ;
     }
 
