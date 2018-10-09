45da6755ced069d7ca7475cd54e613b4f707effc
Mateusz Bątruk <mateuszb254@gmail.com>
2018-10-09 20:54:19 +0200

    Prevent sending multiple requests in the same time

--- public/js/ranking_players.jquery.js
+++ public/js/ranking_players.jquery.js
@@ -1,11 +1,21 @@
 $(function () {
-    $('#search-form').submit(function (e) {
+    let form = $('#search-form');
+    $(form).submit(function (e) {
         e.preventDefault();
 
-        var name = $('input[name=search]').val();
-        var path = $(this).attr('action');
+        let name = $('input[name=search]').val();
 
-        var request_path = path + '/' + name;
+        if($(this).hasClass('stopped')) {
+            return;
+        }
+
+        if(name == '') return;
+
+        $(this).addClass('stopped');
+
+        let path = $(this).attr('action');
+
+        let request_path = path + '/' + name;
 
         $.ajax({
             dataType: 'json',
@@ -15,15 +25,17 @@ $(function () {
                 $('.rank-pagination').remove();
             },
             success: function (data) {
-                var tableRow = '<tr> <td class="rank">- </td> <td class="player">' + data.name + '</td> <td class="kingdom"><img src="' + kingdom(data.kingdom) + '" alt=""> </td> <td class="lvl">' + data.level + ' </td> </tr>';
+                let tableRow = '<tr> <td class="rank">- </td> <td class="player">' + data.name + '</td> <td class="kingdom"><img src="' + kingdom(data.kingdom) + '" alt=""> </td> <td class="lvl">' + data.level + ' </td> </tr>';
 
                 $('.lower table').append(tableRow);
+                $(form).removeClass('stopped');
             },
             statusCode: {
                 404: function () {
-                    var tableRow = '<tr> <td class="rank"></td> <td class="player">Nie znaleziono postaci.</td> <td class="kingdom"> <img src="content/kingdom.png" alt=""> </td> <td class="lvl"></td></tr>';
+                    let tableRow = '<tr> <td class="rank"></td> <td class="player">Nie znaleziono postaci.</td> <td class="kingdom"> <img src="content/kingdom.png" alt=""> </td> <td class="lvl"></td></tr>';
 
                     $('.lower table').append(tableRow);
+                    $(form).removeClass('stopped');
                 }
             },
             error: function (e) {
