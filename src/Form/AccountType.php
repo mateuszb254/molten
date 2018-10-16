372676e2594e83e3289799701859cb614803f8be
Mateusz Bątruk <mateuszb254@gmail.com>
2018-10-16 23:35:28 +0200

    Add possibility to change users role by ROLE_GLOBAL_ADMIN

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Account;
+use App\Entity\Role;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -13,6 +15,7 @@ use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountType extends AbstractType
@@ -20,10 +23,12 @@ class AccountType extends AbstractType
     public const DEFAULT_VALUE_PASS = 'password';
 
     private $passwordEncoder;
+    private $authorizationChecker;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -39,6 +44,8 @@ class AccountType extends AbstractType
                 'label' => 'label.email'
             ])->add('code', TextType::class, [
                 'label' => 'label.code',
+            ])->addEventListener(FormEvents::PRE_SET_DATA, [
+                $this, 'onPreSetData'
             ])->addEventListener(FormEvents::POST_SUBMIT, [
                 $this, 'onPostSubmit'
             ]);
@@ -54,6 +61,20 @@ class AccountType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Account::class
         ]);
+    }
+
+    public function onPreSetData(FormEvent $event)
+    {
+        $account = $event->getData();
+        $form = $event->getForm();
+
+        if ($this->authorizationChecker->isGranted('CHANGE_ROLE', $account)) {
+            $form->add('role', EntityType::class, [
+                'choice_translation_domain' => true,
+                'class' => Role::class,
+                'label' => 'roles'
+            ]);
+        }
     }
 
     public function onPostSubmit(FormEvent $event)
