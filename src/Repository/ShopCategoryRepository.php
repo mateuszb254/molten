d56eb9763b3b0f7d974e222d7924eb0fe85becf9
Mateusz Bątruk <mateuszb254@gmail.com>
2018-10-29 21:00:28 +0100

    Extend findAll to fetch all categories ordered by position ASC as default

--- src/Repository/ShopCategoryRepository.php
+++ src/Repository/ShopCategoryRepository.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 /**
  * @method ShopCategory|null find($id, $lockMode = null, $lockVersion = null)
  * @method ShopCategory|null findOneBy(array $criteria, array $orderBy = null)
- * @method ShopCategory[]    findAll()
  * @method ShopCategory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ShopCategoryRepository extends ServiceEntityRepository
@@ -19,14 +18,22 @@ class ShopCategoryRepository extends ServiceEntityRepository
         parent::__construct($registry, ShopCategory::class);
     }
 
+    public function findAll()
+    {
+        return $this->findBy([], [
+            'position' => 'ASC'
+        ]);
+    }
+
     /**
-     * @return ShopCategory[] Returns an array of ShopCategory objects with ShopCategory::STATUS_ACTIVE value
+     * @return ShopCategory[] Returns an array of ShopCategory objects with ShopCategory::STATUS_ACTIVE value sorted ASC
      */
     public function findAllActiveCategories()
     {
         $query = $this->createQueryBuilder('c')
             ->where('c.status = :status')
             ->setParameter(':status', ShopCategory::STATUS_ACTIVE)
+            ->orderBy('c.position', 'ASC')
             ->getQuery();
 
         return $query->execute();
