6ec075e531bd6aee0761e287ad124fc5d308f180
Mateusz Bątruk <mateuszb254@gmail.com>
2018-10-29 20:59:48 +0100

    Create ItemRepository

---
+++ src/Repository/ItemRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Item;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Item|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Item|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Item[]    findAll()
+ * @method Item[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ItemRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Item::class);
+    }
+
+    /**
+     * @param string $name
+     * @return Item|null
+     */
+    public function findItemByName(string $name)
+    {
+        return $this->findOneBy([
+            'name' => $name
+        ]);
+    }
+}
